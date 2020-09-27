7102ac28ab2691599e91275e2c54ccb256c74eef
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2020-09-27 11:43:05 +0530

    one hidden layer completed

--- oneHiddenLayer.py
+++ oneHiddenLayer.py
@@ -10,7 +10,6 @@ Dataset = Data.Dataset
 Target = Data.Target
 Testset = Data.Testset
 Expectedoutput = Data.Expectedoutput
-#initialize weight and bias with random values
 
 #all the values of learning rate
 alpha = [0.01,0.05,0.1,0.2,0.4,0.8]
@@ -22,7 +21,7 @@ def sigmoid(x):
 #function to train Network
 def train(W,Wb,V,Vb,Dataset,Target,alpha):
     
-    condition = True
+    condition = True    
     epoch = 0
     num_nodes_input = len(W)
     num_nodes_hidden = len(Wb)
@@ -33,16 +32,16 @@ def train(W,Wb,V,Vb,Dataset,Target,alpha):
     delta_output = [0]*num_nodes_output
     delta_hidden = [0]*num_nodes_hidden
     
-
-    while condition is True:
+    while condition is True:                 #continue while error is < 0.01 0r epoch < 10000
         error = 0 
         epoch += 1
-        for num in range(15):
+        for num in range(15):                #for each input image vector
 
-            X=Dataset[num]
+            X=Dataset[num]                  
             T=Target[num]
 
-            for j in range(num_nodes_hidden):
+                                                     #forward Propogation
+            for j in range(num_nodes_hidden):        #caluculate Z
                 Zinj = 0
                 for i in range(num_nodes_input):
                     Zinj += X[i]*W[i][j]
@@ -50,7 +49,7 @@ def train(W,Wb,V,Vb,Dataset,Target,alpha):
                 Zinj = Wb[j] + Zinj
                 Z[j] = sigmoid(Zinj)
             
-            for j in range(num_nodes_output):
+            for j in range(num_nodes_output):        #calculate Y
                 Yinj = 0
                 for i in range(num_nodes_hidden):
                     Yinj += Z[i]*V[i][j]
@@ -58,26 +57,26 @@ def train(W,Wb,V,Vb,Dataset,Target,alpha):
                 Yinj = Vb[j] + Yinj
                 Y[j] = sigmoid(Yinj)
 
-            finalop[num] = Y.copy()
+            finalop[num] = Y.copy()                  #stores output for each of 15 image vectors
 
-            for i in range(3):
+            for i in range(3):                        #sum square error
                 error+=(finalop[num][i] - T[i])**2
             
-            for k in range(num_nodes_output):
-                delta_output[k] = (T[k]-Y[k])*(Y[k])*(1-Y[k])
+            for k in range(num_nodes_output):                        # back Propogation
+                delta_output[k] = (T[k]-Y[k])*(Y[k])*(1-Y[k])        #errors for output nodes
 
-            for j in range(num_nodes_hidden):
+            for j in range(num_nodes_hidden):                       
                 deltaj = 0
                 for k in range(num_nodes_output):
                     deltaj += delta_output[k]*V[j][k]
-                delta_hidden[j] = deltaj
+                delta_hidden[j] = deltaj          
             
             for j in range(num_nodes_hidden):
-                delta_hidden[j] = delta_hidden[j]*(Z[j])*(1-Z[j])
-
-            for i in range(num_nodes_input):
+                delta_hidden[j] = delta_hidden[j]*(Z[j])*(1-Z[j])      #errors for hidden nodes
+ 
+            for i in range(num_nodes_input):                           #udate weights and Bias
                 for j in range(num_nodes_hidden):
-                    W[i][j]+=alpha*delta_hidden[j]*X[i]    
+                    W[i][j]+=alpha*delta_hidden[j]*X[i]                
             
             for i in range(num_nodes_hidden):
                 Wb[i]+=alpha*delta_hidden[i]
@@ -89,12 +88,12 @@ def train(W,Wb,V,Vb,Dataset,Target,alpha):
             for i in range(num_nodes_output):
                 Vb[i]+=alpha*delta_output[i]
 
-        if error<0.01 or epoch>10000:
+        if error<0.01 or epoch>10000:                                    #stopping condition
             condition=False        
     print(f"Epoch is {epoch}")
 
 
-
+#function to test the network
 def test(W,Wb,V,Vb,Testset):
 
     Z = [0]*num_nodes_hidden
@@ -116,13 +115,13 @@ def test(W,Wb,V,Vb,Testset):
             Yinj = Vb[j] + Yinj
             Y[j] = sigmoid(Yinj)
             
-            if Y[j]<0.5:
+            if Y[j]<0.5:                             #approximate values based on output
                 Yconverted[j] = 0
             else:
                 Yconverted[j] = 1
 
 
-        if Yconverted[0]==0 and Yconverted[1]==0 and Yconverted[2]==1:
+        if Yconverted[0]==0 and Yconverted[1]==0 and Yconverted[2]==1:        #output cases
             result = "K"
         elif Yconverted[0]==0 and Yconverted[1]==1 and Yconverted[2]==0:
             result = "D"
@@ -137,8 +136,9 @@ def test(W,Wb,V,Vb,Testset):
         
 
     
-
+#test for all the learning rates
 for i in range(6):
+    #initialize with random values
     W = np.random.rand(num_nodes_input,num_nodes_hidden)
     W = np.multiply(W,0.01)
     Wb = np.random.rand(num_nodes_hidden)
